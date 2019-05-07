a4a60e61161b9ac321e481bd5786d34ab3d9fbeb
Roman Bulinski <aboandrus@gmail.com>
2019-05-07 08:40:16 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
+import java.util.HashMap;
+
 public class Main {
+
+
+    static int stray(int[] numbers) {
+        int numReturn=0;
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for (int num : numbers ){
+            if ( !hashmap.containsKey(num)){
+                hashmap.put( num,1);
+            }
+            if ( hashmap.containsKey(num)){
+                int counter = hashmap.get(num);
+                hashmap.put( num,counter+1);
+            }
+        }
+        for ( Integer number : hashmap.keySet() ) {
+            if(  hashmap.get(number) == 1  ){
+                numReturn = number;
+            }
+        }
+        return numReturn;
+    }
+
+
+
 }
